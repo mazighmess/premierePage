d167b1f97f285d970b41ac07f6448b4539e2bf82
mazighmess <mazighmessaoudi@outlook.fr>
2023-12-01 12:04:57 +0100

    tictacdoefin

--- script/games.js
+++ script/games.js
@@ -10,6 +10,9 @@ function menuChange() {
         case '1':
             fairNumber()
             break 
+        case'2' : 
+        ticTacDoe()
+            break
         default:
             reset()
             break
@@ -77,15 +80,125 @@ function fairNumber() {
 }
 
 
+function reset () {
     
-/* on configure les évènements*/
-/*
-    if(menu.value == '0'){
-        zone.style.backgroundColor="bleue"
-    }else if (menu.value == '1'){
-        zone.style.backgroundColor="red"
+}
+
+
+function fairNumber () {
+}
+    
+function ticTacDoe () {
+    reset ()
+    console.log ('jeu du morpion')
+
+    /*déclaration des variables*/
+    let squares = []
+    let isActive = []
+    let score = []
+    let redPlayer = true; 
+
+
+    let infoPanel = document.createElement('div')
+    infoPanel.classList.add('infoPanel')
+
+    let grid = document.createElement('div')
+    grid.classList.add('grid')
+
+    zone.appendChild(infoPanel)
+    zone.appendChild(grid)
+    zone.classList.add('ttdZone')
+
+    /* création de la grille*/
+
+    for(let i = 0 ; i<9 ; i++) {
+        let square = document.createElement
+        ('div')
+        square.classList.add('square')
+        grid.appendChild(square)
+        squares.push(square)
+        isActive.push(true)
+        score.push(0)
     }
-*/
-/*on configure les évènements*/
+
+    for(let i= 0; i<9; i++) {
+        squares[i].addEventListener('click' , squareClick.bind(squares[i], i))
+    }
+
+    console.log(squares, isActive)
+
+    function squareClick(squareNumber){
+        if(isActive[squareNumber]){
+            if(redPlayer) {
+                this.style.backgroundImage = 'url(../assets/croixNoir.png)' 
+                score[squareNumber] = 1 
+            } else {
+                this.style.backgroundImage = 'url(../assets/rondNoir.png)' 
+                score[squareNumber] = 4
+            } 
+
+            isActive[squareNumber] = false
+        
+            redPlayer = !redPlayer
+            checkVictory()
+            console.log(score)
+
+        }
+    }
+
+    function checkVictory(){
+        let lineScore = [
+            score[0] + score[1] + score[2],
+            score[3] + score[4] + score[5],
+            score[6] + score[7] + score[8],
+            score[0] + score[3] + score[6],
+            score[1] + score[4] + score[7],
+            score[2] + score[5] + score[8],
+            score[0] + score[4] + score[8],
+            score[2] + score[4] + score[6],
+        ]
+
+        let endGameMessage = ""
+        if(lineScore.includes(3)) {
+                endGameMessage = " Victoire Rouge"
+                endGameMessage(endGameMessage)
+            }else if (lineScore.includes(12)){
+                endGameMessage = "Victoire vert"
+                endGameMessage(endGameMessage)
+            }else if (!isActive.includes(true)) {
+                endGameMessage = "Egalité"
+                endGameMessage(endGameMessage)
+            }
+                
+            
+        
+
+        console.log(lineScore, endGameMessage)
+
+    }
+
+    function endGame(endGameMessage) {
+        for(let i = 0; i<8; i++) {
+            isActive[i] = false
+
+        }
+        let endOverGame = document.createElement('h2')
+        gameOverGame.classList.add('gameOver')
+        gameOverGame.innerHTML = endGameMessage
+
+        infoPanel.appendChild (gameOverGame)
+
+        let restartBut = document.createElement('button')
+        restartBut.addEventListener('click' , ticTacDoe)
+
+        infoPanel.appendChild(restartBut)
+
+    }
+}
+
+
+
+/* on configure les évènements*/
+
 
 menu.addEventListener("change" , menuChange)
